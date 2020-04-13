4462afeb2755567f0f805ea0c6e35cd129ab7bda
Igor Grillo Peternella <igor.feq@gmail.com>
2020-04-13 13:18:24 -0300

    Added http 201 and http 400 book creation tests.

--- core/tests.py
+++ core/tests.py
@@ -30,7 +30,7 @@ class BookTestCase(TestCase):
         self.assertEqual(saved_book.book_store_id, self.book_store_1.id)
 
 
-class BookStoreApiTestCase(TestCase):
+class RestApiTestCase(TestCase):
 
     # Test case related variables
     @classmethod
@@ -45,6 +45,7 @@ class BookStoreApiTestCase(TestCase):
             book_store=cls.book_store_1, name="book_2", qty=4
         )
 
+    # HTTP GET -- with and without query strings -- assertion with HTTP 200
     def test_should_get_a_list_of_stores_http_200(self):
         # self.client.get(reverse("book-store-endpoint-list"))
         response = self.client.get("/api/v1/book-store/")
@@ -55,6 +56,16 @@ class BookStoreApiTestCase(TestCase):
         self.assertEqual(json_response["count"], 2)
         self.assertEqual(json_response["results"][0]["cnpj"], "cnpj_1")
 
+    def test_should_filter_book_stores_by_book_store_name_query_string_http_200(self):
+        query_string_params = {"cnpj": "cnpj_2"}
+
+        response = self.client.get("/api/v1/book-store/", query_string_params)
+        json_response = response.json()
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(json_response["count"], 1)
+        self.assertEqual(json_response["results"][0]["cnpj"], "cnpj_2")
+
     def test_should_get_a_specific_store_http_200(self):
         response = self.client.get(f"/api/v1/book-store/{self.book_store_2.id}/")
         json_response = response.json()
@@ -62,3 +73,40 @@ class BookStoreApiTestCase(TestCase):
         # assertions
         self.assertEqual(response.status_code, 200)
         self.assertEqual(json_response["cnpj"], "cnpj_2")
+
+    # HTTP POST -- assertion with HTTP 201
+    def test_should_create_book_from_store_1_http_201(self):
+        book_data = {
+            "name": " book_3",
+            "qty": 3,
+            "book_store": str(self.book_store_1.id),
+        }
+
+        response = self.client.post(f"/api/v1/book/", book_data)
+        json_response = response.json()
+
+        # assertions
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(json_response["name"], "book_3")
+        self.assertEqual(
+            Book.objects.filter(name="book_3", book_store__name="store_1").count(), 1
+        )  # created successfully!
+
+    # HTTP POST -- malformed -- assertion with HTTP 400
+    def test_should_not_create_book_from_store_1_due_to_malformed_request_http_400(
+        self,
+    ):
+        book_data = {
+            # "name": " book_3",
+            "qty": 3,
+            "book_store": str(self.book_store_1.id),
+        }
+
+        response = self.client.post(f"/api/v1/book/", book_data)
+        json_response = response.json()
+
+        # assertions
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(
+            Book.objects.filter(name="book_3", book_store__name="store_1").count(), 0
+        )  # NO creation
